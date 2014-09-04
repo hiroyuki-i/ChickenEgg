1a45a78a50e3daba04196a5d4b1ee9bb670e3211
Hiroyuki Iijima <f.work111@gmail.com>
2014-09-04 16:32:04 +0900

    フロア、左右のコライダ　開始時自動配置

---
+++ Assets/Scripts/Wall.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var WallRight : GameObject;
+var WallLeft : GameObject;
+var Floor : GameObject;
+
+function Start(){
+	var windowSize : Vector2 = Camera.main.ViewportToWorldPoint(new Vector2(1,1));
+	var windowSizeMinus : Vector2 = windowSize * -1;
+	WallLeft.transform.position = new Vector2(windowSize.x , windowSizeMinus.y);
+	WallRight.transform.position = windowSizeMinus;
+	Floor.transform.position = windowSizeMinus;
+}
